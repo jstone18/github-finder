aa7e15059dbe1eb5188135bc51bd60c220b68717
jstone18 <jason.stone0808@gmail.com>
2019-11-11 17:53:24 -0500

    Add lifecycle method to fetch data from github api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true })
+
+    fetch("https://api.github.com/users")
+      .then(res => res.json())
+      .then(data => this.setState({ users: data, loading: false })
+      )
+  }
+
   render() {
+    console.log(this.state.users)
     return (
       <div className="App">
       <Navbar />
       <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>
       </div>
     );
