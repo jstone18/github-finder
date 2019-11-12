0deed023b9384479f11ee1ad5216dc326f23d616
jstone18 <jason.stone0808@gmail.com>
2019-11-11 20:24:19 -0500

    Destructure state being passed in components

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     }
     
     render() {
+      const { users, loading } = this.state
 
     return (
       <div className="App">
@@ -35,8 +36,8 @@ class App extends Component {
         <Search 
           searchUsers={this.searchUsers} 
           clearUsers={this.clearUsers} 
-          showClear={this.state.users.length > 0 ? true : false} />
-        <Users loading={this.state.loading} users={this.state.users} />
+          showClear={users.length > 0 ? true : false} />
+        <Users loading={loading} users={users} />
       </div>
       </div>
     );
