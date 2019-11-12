6ec6aec98abaeac10c9fac23abaa914347b4efcf
jstone18 <jason.stone0808@gmail.com>
2019-11-11 19:26:36 -0500

    Create Search component with search form.

---
+++ src/components/users/Search.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  state = {
+    text: ''
+  }
+  
+  handleChange = (e) => this.setState({ text: e.target.value})
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.handleChange} />
+          <input type="submit" value="Search" className="btn btn-dark btn-block" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
