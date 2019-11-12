7be3b9038d824502a7b560ea29a0bf7cc71716b3
jstone18 <jason.stone0808@gmail.com>
2019-11-12 00:30:41 -0500

    Create getUser(), add user route and Link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -10,6 +11,7 @@ import About from './components/pages/About';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -26,6 +28,18 @@ class App extends Component {
       )
     }
 
+    // Get single Github user
+    getUser = (username) => {
+      this.setState({ loading: true })
+
+    fetch(`https://api.github.com/users/${username}?
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+      .then(res => res.json())
+      .then(data => this.setState({ user: data, loading: false })
+      )
+    }
+
     clearUsers = () => {
       this.setState({ users: [], loading: false })
     }
@@ -38,7 +52,7 @@ class App extends Component {
     }
     
     render() {
-      const { users, loading } = this.state
+      const { users, user, loading } = this.state
 
     return (
       <Router>
@@ -61,6 +75,14 @@ class App extends Component {
                 )}  
               />
               <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={props => (
+                <User 
+                  { ...props } 
+                  getUser={this.getUser} 
+                  user={user}
+                  loading={loading} 
+                />
+              )} />
             </Switch>
           </div>
         </div>
